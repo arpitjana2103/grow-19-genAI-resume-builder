fa60b61668b2dd6370b882035909280cb3a6dd0b
arpit jana <arpitjana2103@gmail.com>
2026-07-18 13:24:06 +0530

    [fe] style : appView designed

--- Frontend-CVLens/src/features/ai/components/AppView.tsx
+++ Frontend-CVLens/src/features/ai/components/AppView.tsx
@@ -1,27 +1,54 @@
 import {
+    Asterisk02Icon,
     Briefcase07Icon,
+    Cancel01Icon,
     DocumentAttachmentIcon,
     Job,
+    MagicWand01Icon,
     Male02Icon,
+    Pdf01Icon,
     Pdf02Icon,
 } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { useState } from "react";
+import { useState, type ChangeEvent, type DragEvent } from "react";
+import toast from "react-hot-toast";
 
+import geminiImg from "@/assets/gemini.png";
 import AppHeading from "@/components/shared/AppHeading";
+import MyButton from "@/components/shared/MyButton";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { cn, formatFileSize } from "@/lib/utils";
 
 export default function AppView() {
     return (
         <>
             <AppHeading />
-            <div className="mx-auto mt-8 h-160 w-full border-2 border-border bg-primary p-6">
-                <div className="flex h-full">
+            <div className="mx-auto mt-8 flex h-180 w-full flex-col border-2 border-border bg-primary p-6">
+                <div className="flex grow">
                     <JobDescription />
                     <Profile />
                 </div>
-                <div className="hidden">
+                <div className="mt-6 flex items-center justify-between">
                     <p>AI Powered Strategy Generation • 30s Approx </p>
-                    <button>Generate My Interview Strategy</button>
+
+                    <div className="group cursor-pointer bg-background p-1.5">
+                        <Button className="!active:translate-y-0 cursor-pointer border-2 border-blue-300 bg-background px-3 py-4 text-lg text-foreground hover:bg-background/90">
+                            <span className="font-head">
+                                Generate With{" "}
+                                <span className="font-gemini font-medium text-blue-400">
+                                    Gemini
+                                </span>
+                            </span>
+                            <span>
+                                <img
+                                    className="h-6 w-6 group-hover:animate-spin"
+                                    src={geminiImg}
+                                    alt=""
+                                />
+                            </span>
+                        </Button>
+                    </div>
                 </div>
             </div>
         </>
@@ -37,7 +64,10 @@ function JobDescription() {
                     <span className="font-head text-lg">Target Job Description</span>
                 </p>
             </div>
-            <textarea className="grow resize-none bg-foreground p-3 text-base text-background focus:outline-none"></textarea>
+            <textarea
+                placeholder="Paste the full job description here ... "
+                className="grow resize-none bg-foreground p-3 text-base text-background focus:outline-none"
+            ></textarea>
         </div>
     );
 }
@@ -59,35 +89,143 @@ function SelfDescription() {
     return (
         <div className="flex grow flex-col">
             <p className="mt-4 mb-2 font-head text-lg">Self Description</p>
-            <textarea className="w-full grow resize-none bg-foreground p-3 text-base text-background focus:outline-none"></textarea>
+            <div className="relative grow">
+                <textarea
+                    className="h-full w-full resize-none bg-foreground p-3 text-base text-background focus:outline-none"
+                    placeholder="Briefly describe your experience, key skills, and years of experience."
+                ></textarea>
+                <div className="absolute right-4 bottom-3 mt-2 flex cursor-pointer items-center justify-end gap-3 bg-foreground p-2">
+                    <label
+                        htmlFor="check-selfdes"
+                        className="cursor-pointer font-semibold text-background select-none"
+                    >
+                        Use Resume data as self-description
+                    </label>
+                    <Checkbox
+                        id="check-selfdes"
+                        className="size-4 rounded-none border-none bg-background ring-2 ring-foreground data-checked:bg-background data-checked:text-foreground"
+                    />
+                </div>
+            </div>
         </div>
     );
 }
 
 function ResumeUpload() {
-    const [file, setFile] = useState(null);
+    const [file, setFile] = useState<File | null>(null);
+    const [drag, setDrag] = useState(false);
 
-    function handleSelect() {}
+    function handleSelect(e: ChangeEvent<HTMLInputElement>) {
+        e.preventDefault();
+        if (!e.target.files || e.target.files.length === 0) return;
+        const fileData = e.target.files[0];
+        const fileType = fileData.type;
+        if (fileType !== "application/pdf") {
+            toast.error("Only PDF files are allowed");
+            return;
+        }
+        setFile(fileData);
+    }
+
+    function handleRemoveFile() {
+        setFile(null);
+    }
+
+    function handleDragEnter(e: DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        setDrag(true);
+    }
+
+    function handleDragLeave(e: DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        setDrag(false);
+    }
+
+    function handleDragOver(e: DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        setDrag(true);
+    }
+
+    function handleDrop(e: DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        setDrag(false);
+        const fileData = e.dataTransfer.files[0];
+        const fileType = fileData.type;
+        if (fileType !== "application/pdf") {
+            toast.error("Only PDF files are allowed");
+            return;
+        }
+        setFile(fileData);
+    }
 
     return (
         <div>
             <p className="mb-2 font-head text-lg">Upload Resume</p>
             <label
                 htmlFor="resume-upload"
-                className="block w-full cursor-pointer bg-foreground p-2"
+                className="block h-40 w-full cursor-pointer bg-foreground p-2"
             >
-                <div className="flex flex-col items-center justify-center border-2 border-dashed border-primary p-6 text-background">
-                    <span>
-                        <HugeiconsIcon
-                            icon={DocumentAttachmentIcon}
-                            className="h-10 w-10 text-primary"
-                        />
-                    </span>
-                    <p className="mt-4 text-lg">Click to Upload Resume or Drag & Drop </p>
-                    <p className="mt-1 text-sm text-background/70">{`File Type : PDF | Max Size : 3MB | Max PageCount : 2`}</p>
-                </div>
+                {!file && (
+                    <div
+                        onDragEnter={handleDragEnter}
+                        onDragLeave={handleDragLeave}
+                        onDragOver={handleDragOver}
+                        onDrop={handleDrop}
+                        className={cn(
+                            "flex h-full items-center justify-center border-2 border-dashed border-primary text-background",
+                            drag && "border-solid",
+                        )}
+                    >
+                        <div
+                            className={cn(
+                                "flex flex-col items-center justify-center",
+                                drag && "opacity-0",
+                            )}
+                        >
+                            <span>
+                                <HugeiconsIcon
+                                    icon={Pdf02Icon}
+                                    strokeWidth={1}
+                                    className="h-12 w-12 text-primary"
+                                />
+                            </span>
+                            <p className="mt-2 text-lg">Click to Upload Resume or Drag & Drop </p>
+                            <p className="mt-1 text-sm text-background/70">{`File Type : PDF | Max Size : 3MB | Max PageCount : 2`}</p>
+                        </div>
+                    </div>
+                )}
+                {file && (
+                    <div className="flex h-full items-center justify-center gap-4 border-2 border-dashed border-primary text-background/95">
+                        <button
+                            onClick={handleRemoveFile}
+                            className="cursor-pointer border-2 border-primary"
+                        >
+                            <HugeiconsIcon
+                                icon={Cancel01Icon}
+                                strokeWidth={2}
+                                className="h-6 w-6 text-primary"
+                            />
+                        </button>
+                        <div>
+                            <HugeiconsIcon
+                                icon={Pdf01Icon}
+                                strokeWidth={0.8}
+                                className="h-20 w-20 text-primary"
+                            />
+                        </div>
+                        <div className="">
+                            <p className="text-xl">
+                                {file.name.slice(0, 30)}
+                                {file.name.length > 30 ? "..." : ""}
+                            </p>
+                            <span className="text-background/70">{formatFileSize(file.size)}</span>
+                        </div>
+                    </div>
+                )}
             </label>
-            <input type="file" onSelect={handleSelect} id="resume-upload" className="hidden" />
+            {!file && (
+                <input type="file" onChange={handleSelect} id="resume-upload" className="hidden" />
+            )}
         </div>
     );
 }
--- Frontend-CVLens/src/lib/utils.ts
+++ Frontend-CVLens/src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function formatFileSize(bytes: number): string {
+    if (bytes < 1024) {
+        return `${bytes} B`;
+    }
+    if (bytes < 1024 * 1024) {
+        return `${(bytes / 1024).toFixed(2)} KB`;
+    }
+    if (bytes < 1024 * 1024 * 1024) {
+        return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+    }
+    return `${(bytes / (1024 * 1024 * 1024)).toFixed(2)} GB`;
 }
